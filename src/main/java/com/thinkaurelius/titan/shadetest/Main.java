9eb49f0b5cfd6da302dc7e37f1cbc4b5f94d5ea2
Dan LaRocque <dalaro@hopcount.org>
2013-12-04 22:44:22 -0500

    Fix misleading strings in main class

--- src/main/java/com/thinkaurelius/titan/shadetest/Main.java
+++ src/main/java/com/thinkaurelius/titan/shadetest/Main.java
@@ -3,6 +3,6 @@ package com.thinkaurelius.titan.shadetest;
 public class Main {
 	public static void main(String args[]) {
 		System.out.println("Current Titan version: " + com.thinkaurelius.titan.graphdb.configuration.TitanConstants.VERSION);
-		System.out.println("Current Titan version: " + titan03.com.thinkaurelius.titan.graphdb.configuration.TitanConstants.VERSION);
+		System.out.println("Titan03 version: " + titan03.com.thinkaurelius.titan.graphdb.configuration.TitanConstants.VERSION);
 	}
 }
