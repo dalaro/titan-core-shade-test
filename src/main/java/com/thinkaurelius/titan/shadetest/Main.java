320abea9930064ab75faa71239c9a1ebf73863e0
Dan LaRocque <dalaro@hopcount.org>
2013-12-04 22:39:53 -0500

    Adding pom and proof of concept main class

---
+++ src/main/java/com/thinkaurelius/titan/shadetest/Main.java
@@ -0,0 +1,8 @@
+package com.thinkaurelius.titan.shadetest;
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("Current Titan version: " + com.thinkaurelius.titan.graphdb.configuration.TitanConstants.VERSION);
+		System.out.println("Current Titan version: " + titan03.com.thinkaurelius.titan.graphdb.configuration.TitanConstants.VERSION);
+	}
+}
